949dce0587e1a5c457edcff6a1b88f7cc710ae20
Benjamin Ejike <benmma13@gmail.com>
2022-10-15 19:35:47 +0400

    Add functionalities for the missions component

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,31 +1,83 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getMissions } from './Redux/Mission/missions';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMissions, getStoredMissions, updateStatus } from "./Redux/Mission/missions";
+import styles from './Mission.module.css';
 
-function Mission() {
+function Missions() {
   const dispatch = useDispatch();
+  const data = localStorage.getItem('mission_Data');
+  const getMissionsData = () => {
+    data ? dispatch(getStoredMissions()) : dispatch(getMissions());
+  };
+
   useEffect(() => {
-    dispatch(getMissions());
+    getMissionsData();
   }, []);
+
   const missions = useSelector((state) => state.missions);
   return (
-    <div className="table">
-      <div className="table-headers">
-        <h2 className="name">Mission</h2>
-        <h2 className="des">Description</h2>
-        <h2 className="status">Status</h2>
+    <div className={styles.table}>
+      <div className={styles.tableHeaders}>
+        <h2 className={styles.name}>Mission</h2>
+        <h2 className={styles.des}>Description</h2>
+        <h2 className={styles.status}>Status</h2>
+        <p className={styles.empty} />
       </div>
-      {missions.map((item) => (
-        <div className="mission" key={item.id}>
-          <h2>{item.name}</h2>
-          <p>{item.description}</p>
-          <button type="button">Join mission</button>
-          <button type="button">Leave mission</button>
-        </div>
-      ))}
+      {missions.map((item) => {
+        return (
+          <Mission
+            name={item.name}
+            id={item.id}
+            key={item.id}
+            reserved={item.reserved}
+            description={item.description}
+          />
+        );
+      })}
     </div>
-  );
+  )
 }
 
-export default Mission;
+function Mission({ name, id, description, reserved }) {
+  const dispatch = useDispatch();
+  const btnText = (!reserved) ? 'Join Mission' : 'Leave Mission';
+  const statusTxt = (!reserved) ? 'NOT A MEMBER' : 'Active member';
+
+  const btnStyle = (reserved) ? {
+    backgroundColor: 'transparent',
+    color: '#dc3645',
+    border: '1px solid #dc3645',
+  }
+    : {
+      backgroundColor: 'transparent',
+      color: '#343a40',
+      border: '1px solid #343a40',
+    };
+  
+  const tagStyle = (reserved) ? { backgroundColor: '#18a2b8' } : { backgroundColor: '#6d757d' };
+
+  return (
+
+    <div className={styles.mission}>
+      <h2>{name}</h2>
+      <p className={styles.description}>{description}</p>
+      <div className={styles.tagContainer}>
+        <p style={tagStyle} className={styles.tag}>{statusTxt}</p>
+      </div>
+      <div className={styles.btnContainer}>
+        <button
+          style={btnStyle}
+          type="button"
+          className={styles.missionBtn}
+          onClick={() => dispatch(updateStatus(id))}
+        >
+          {btnText}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Missions;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,22 +1,32 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import styles from './Profile.module.css';
 
 export default function Profile() {
   const rockets = useSelector((state) => state.rockets);
+  const missions = useSelector((state) => state.missions);
   const resRockets = rockets.filter((rocket) => rocket.reserved);
+  const resMissions = missions.filter((mission) => mission.reserved);
 
   return (
     <div className={styles.myProfile}>
       <div className={styles.missions}>
         <h2>Missions</h2>
-        <div className={styles.missionList} />
+        <div className={styles.missionList}>
+        {resMissions.map((mission) => {
+            return (
+              <p key={mission.id}>{mission.name}</p>
+            );
+          })}
+        </div>
       </div>
       <div className={styles.rockets}>
         <h2>Rockets</h2>
         <div className={styles.rocketList}>
-          {resRockets.map((rocket) => (
-            <p key={rocket.id}>{rocket.name}</p>
-          ))}
+          {resRockets.map((rocket) => {
+            return (
+              <p key={rocket.id}>{rocket.name}</p>
+            );
+          })}
         </div>
       </div>
     </div>
